bef19b99790019f6f052841145ade90fd3b07d5e
Daniel Moura <daniel.tecnicop@gmail.com>
2024-08-06 16:40:32 -0400

    fix/Corrigido erro ao pesquisar por id

--- src/main/java/com/daniel/brigadeiro/controller/ProdutosController.java
+++ src/main/java/com/daniel/brigadeiro/controller/ProdutosController.java
@@ -16,52 +16,52 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.daniel.brigadeiro.model.Clientes;
-import com.daniel.brigadeiro.model.DTO.ClientesDTO;
-import com.daniel.brigadeiro.service.ClientesService;
+import com.daniel.brigadeiro.model.Produtos;
+import com.daniel.brigadeiro.model.DTO.ProdutosDTO;
+import com.daniel.brigadeiro.service.ProdutosService;
 
 import jakarta.validation.Valid;
 
 @Controller
-@RequestMapping(value = "/clientes")
-public class ClienteController {
+@RequestMapping(value = "/produtos")
+public class ProdutosController {
 
 	@Autowired
-	private ClientesService clientesService;
+	private ProdutosService produtoService;
 	
-	 public ClienteController(ClientesService clientesService) {
-	        this.clientesService = clientesService;
+	 public ProdutosController(ProdutosService produtoService) {
+	        this.produtoService = produtoService;
 	    }
 	
-	@GetMapping(value = "/{id}")
-	public ResponseEntity<ClientesDTO> findById(@PathVariable Long id){
-		Clientes obj = this.clientesService.findById(id);
-		return ResponseEntity.ok().body(new ClientesDTO(obj));
+	 @GetMapping(value = "/{id}")
+	public ResponseEntity<ProdutosDTO> findById(@PathVariable Long id){
+		Produtos obj = this.produtoService.findById(id);
+		return ResponseEntity.ok().body(new ProdutosDTO(obj));
 	}
 	
 	  @GetMapping
-	    public ResponseEntity<List<ClientesDTO>> findAll(){
-	        List<Clientes> list = clientesService.findAll();
-	        List<ClientesDTO> listDTO = list.stream().map(ClientesDTO::new).collect(Collectors.toList());
+	    public ResponseEntity<List<ProdutosDTO>> findAll(){
+	        List<Produtos> list = produtoService.findAll();
+	        List<ProdutosDTO> listDTO = list.stream().map(ProdutosDTO::new).collect(Collectors.toList());
 	        return ResponseEntity.ok().body(listDTO);
 	    }
 	  
 	  @PostMapping
-	    public ResponseEntity<Void> create(@Valid @RequestBody ClientesDTO objDTO){
-	        Clientes obj = clientesService.create(objDTO);
+	    public ResponseEntity<Void> create(@Valid @RequestBody ProdutosDTO objDTO){
+	        Produtos obj = produtoService.create(objDTO);
 	        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 	        return ResponseEntity.created(uri).build();
 	    }
 	     
 	     @PutMapping(value = "/{id}")
-	     public ResponseEntity<ClientesDTO> update(@PathVariable Long id, @Valid @RequestBody ClientesDTO objDTO){
-	    	 Clientes obj = clientesService.update(id, objDTO);
-	    	 return ResponseEntity.ok().body(new ClientesDTO(obj));
+	     public ResponseEntity<ProdutosDTO> update(@PathVariable Long id, @Valid @RequestBody ProdutosDTO objDTO){
+	    	 Produtos obj = produtoService.update(id, objDTO);
+	    	 return ResponseEntity.ok().body(new ProdutosDTO(obj));
 	     }
 	     
 	     @DeleteMapping(value = "/{id}")
-	 	public ResponseEntity<ClientesDTO> delete(@PathVariable Long id){
-	    	 clientesService.delete(id);
+	 	public ResponseEntity<ProdutosDTO> delete(@PathVariable Long id){
+	    	 produtoService.delete(id);
 		return ResponseEntity.noContent().build();
 	}
 }
