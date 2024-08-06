da973618a6cf74762f7a7fd2d4bfea4c83b1673e
Daniel Moura <daniel.tecnicop@gmail.com>
2024-08-06 13:49:08 -0400

    Feature/Criado classes de Exception para serviços

---
+++ src/main/java/com/daniel/brigadeiro/service/exception/DataIntegrityViolationException.java
@@ -0,0 +1,15 @@
+package com.daniel.brigadeiro.service.exception;
+
+public class DataIntegrityViolationException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+
+	public DataIntegrityViolationException(String message) {
+        super(message);
+    }
+
+    public DataIntegrityViolationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
