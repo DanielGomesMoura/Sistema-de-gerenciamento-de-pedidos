f8f2ef480c55c664bf2f93d57b729dd3b27ae765
Daniel Moura <daniel.tecnicop@gmail.com>
2024-08-06 16:42:41 -0400

    feature/Implementado campos requeridos para não permitir campos nulos

--- src/main/java/com/daniel/brigadeiro/model/DTO/ClientesDTO.java
+++ src/main/java/com/daniel/brigadeiro/model/DTO/ClientesDTO.java
@@ -4,12 +4,16 @@ import java.io.Serializable;
 
 import com.daniel.brigadeiro.model.Clientes;
 
+import jakarta.validation.constraints.NotNull;
+
 public class ClientesDTO implements Serializable{
 
 private static final long serialVersionUID = 1L;
 	
 	private Long id;
+	@NotNull(message = "O campo EMAIL é requerido")
     private String email;
+	@NotNull(message = "O campo NOME é requerido")
 	private String nome;
 	
 	public Long getId() {
@@ -18,9 +22,11 @@ private static final long serialVersionUID = 1L;
 	public void setId(Long id) {
 		this.id = id;
 	}
+	
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
@@ -34,8 +40,8 @@ private static final long serialVersionUID = 1L;
 	public ClientesDTO(Clientes obj) {
 		super();
 		this.id = obj.getId();
-		this.email = obj.getEmail();
 		this.nome = obj.getNome();
+		this.email = obj.getEmail();
 	}
 	
 	public ClientesDTO() {
