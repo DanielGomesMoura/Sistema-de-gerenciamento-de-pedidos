1dada4d7d5ed4956e6fd762cdbb01928e07f4ecf
Daniel Moura <daniel.tecnicop@gmail.com>
2024-08-29 16:15:25 -0400

    Realizada alteração para retorno ao ser inserido o Pagamento para PagamentosDTO

--- src/main/java/com/daniel/brigadeiro/controller/PagamentosController.java
+++ src/main/java/com/daniel/brigadeiro/controller/PagamentosController.java
@@ -34,10 +34,10 @@ public class PagamentosController {
 		}
 	 
 	 @PostMapping
-	    public ResponseEntity<String> create(@Valid @RequestBody PagamentosDTO objDTO){
+	    public ResponseEntity<PagamentosDTO> create(@Valid @RequestBody PagamentosDTO objDTO){
 		 Pagamentos obj = pagamentosService.create(objDTO);
 	        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-	        return ResponseEntity.created(uri).body("Pedido cadastrado com sucesso!");
+	        return ResponseEntity.created(uri).build();
 	    }
 	 
 	    @PutMapping(value = "/{id}")
