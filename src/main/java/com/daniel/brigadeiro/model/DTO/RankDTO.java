da972a5776306daab91fb04e2eb4c6f057d8053d
DanielMoura <daniel.tecnicop@gmail.com>
2024-09-03 00:23:34 -0400

    Criado RankDTO

---
+++ src/main/java/com/daniel/brigadeiro/model/DTO/RankDTO.java
@@ -0,0 +1,18 @@
+package com.daniel.brigadeiro.model.DTO;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class RankDTO {
+	private String nome;
+	private Long quantidade;
+
+	public RankDTO(String nome, Long quantidade) {
+		this.nome = nome;
+		this.quantidade = quantidade;
+	}
+}
